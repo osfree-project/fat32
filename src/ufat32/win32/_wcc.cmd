63c41ad61c76add06e862797a603cea56bdb52a9
Valery Sedletski <_valerius@mail.ru>
2023-01-02 15:58:21 +0300

    Update _wcc and makefiles.

--- src/ufat32/win32/_wcc.cmd
+++ src/ufat32/win32/_wcc.cmd
@@ -1,3 +1,9 @@
 @echo off
-set path=..\..;%path%
-call buildw.cmd %1 %2 %3 %4 %5 %6 %7 %8 %9
+set root=.
+:loop
+if exist "%root%\tools\mk\all.mk" goto found
+set root=%root%\..
+goto loop
+:found
+set path=%root%\tools\conf\scripts;%path%
+call build %1 %2 %3 %4 %5 %6 %7 %8 %9
